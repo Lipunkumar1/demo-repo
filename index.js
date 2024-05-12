a8a9f0433f348dff63679528c28effbdb5333715
Lipunkumar1 <saumyakantapanda82@gmail.com>
2024-05-12 10:54:57 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const data = {
+  user: {
+    name: "Saumya",
+    bio: "A seltaught fullstack web developer, Top rated plus on Upwork and Top rated on Fiverr"
+  },
+  notes: [{
+    title: "Learn github",
+    description: "Learn to collaborate and contribute to opensource on Github",
+  }]
+}
+  
